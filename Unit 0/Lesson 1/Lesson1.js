b392934d87a02f0f0d321a11e075f340daf1c5f3
Celirous <aj.bellringer@gmail.com>
2026-01-12 10:55:07 +0200

    first commit

---
+++ Unit 0/Lesson 1/Lesson1.js
@@ -0,0 +1,21 @@
+3+3
+let name - "Jon Wxler";
+let name = "Jon Wxler";
+console.log(name)
+class Goat {
+class Goat {eat(foodType) {
+class Goat {eat(foodType) {console.log('I love earing ${foodType}');
+class Goat {eat(foodType) {console.log('I love earing ${foodType}');}
+class Goat {eat(foodType) {console.log('I love earing ${foodType}');}}
+let billy = new Goat();
+billy.eat("tin cans");
+billy.eat("tin cans");
+class Goat {
+class Goat2 {
+class Goat2 {eat(foodType) {
+class Goat2 {eat(foodType) {console.log(`I love eating ${foodType});
+class Goat2 {eat(foodType) {console.log(`I love eating ${foodType}`);}
+class Goat2 {eat(foodType) {console.log(`I love eating ${foodType}`);}}
+let billy = new Goat2():
+let billy2 = new Goat2();
+billy2.eat("tin cans");
---
+++ Unit 0/Lesson 2/universe.js
@@ -0,0 +1,11 @@
+let messages = [
+    "A change of environment can be a good thing!",
+    "You will make it!",
+    "Just run with the code!"
+];
+messages.forEach(message => console.log(message));
+let x = "Universe";
+console.log('Hello, ${x}');
+console.log(`Hello, ${x}');
+console.log(`Hello, ${x}');
+console.log(`Hello, ${x}`);
