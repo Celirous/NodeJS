99a2d197ce692d5a26a2629ed7f6aa4a49ceba31
Celirous <aj.bellringer@gmail.com>
2026-02-16 10:56:08 +0200

    Book 2, chapter 1 & 2

--- Unit 6/Lesson 28/recipe_app/public/js/recipeApp.js
+++ Unit 6/Lesson 28/recipe_app/public/js/recipeApp.js
@@ -3,7 +3,7 @@ $(document).ready(() => { //the $(document) is shorthand for the jquery
     $(".modal-body").html("");  // this will clear it out for you so that it is blank, due to outdated info
     
     try {
-      const results = await $.get("/api/courses?apiToken=recipeT0k3n/$");
+      const results = await $.get("/api/courses?apiToken=recipeT0k3n");
       const data = results.data;
       
       if (!data || !data.courses) return;
@@ -37,7 +37,7 @@ let addJoinButtonListener = () => {
   $(".join-button").click(event => { //Create the event listener for the modal button.
     let $button = $(event.target), // this will store the click event in the $button vat 
       courseId = $button.data("id"); // this will take the element that you clicked on (the course id) and save it in the courseId var
-    $.get(`/api/courses/${courseId}/join`, (results = {}) => { // this will now make a get request to the course using the id, then we make an empty object called results and save it in there 
+    $.get(`/api/courses/${courseId}/join?apiToken=recipeT0k3n`, (results = {}) => { // this will now make a get request to the course using the id, then we make an empty object called results and save it in there 
       let data = results.data; 
       if (data && data.success) { //if we find data in the id we called, it runs this, when we run it, it changes the info on the join button itself
         $button
